c3ba949baa291bef7b72257e63c40470dfa21ffc
Davide Catani <dave.catani@gmail.com>
2022-02-28 09:14:26 +0100

    Added Google Analytics code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { Store } from '@ngrx/store';
 import * as RecipesActions from './store/recipes/recipe.actions';
 import * as GeneralActions from './store/general/general.actions';
 import * as fromApp from './store/app.reducer';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -13,8 +17,9 @@ import * as fromApp from './store/app.reducer';
 export class AppComponent implements OnInit {
   title = 'sesamonero';
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>, private router: Router) {}
   ngOnInit(): void {
+    this.setUpAnalytics();
     this.store.dispatch(new RecipesActions.AnonymousLogin());
     this.store.dispatch(new RecipesActions.FetchRecipes());
   }
@@ -23,5 +28,15 @@ export class AppComponent implements OnInit {
     this.store.dispatch(new GeneralActions.SetCurrentCategory(''));
     this.store.dispatch(new GeneralActions.SetCurrentSearch(''));
     this.store.dispatch(new GeneralActions.SetPage(1));
+  }
+
+  setUpAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'UA-75609499-1', {
+          page_path: event.urlAfterRedirects,
+        });
+      });
   }
 }
